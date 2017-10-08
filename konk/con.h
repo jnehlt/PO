dd100dbd4b4d6cbf1f90496b06699258bdd97a18
sebastianbakala <sebastianbakala@users.noreply.github.com>
2017-10-08 23:29:23 +0200

    Update con.h

--- konk/con.h
+++ konk/con.h
@@ -8,7 +8,7 @@
     #include <stdlib.h>
     #include <string.h>
     
-    int validate(int, const char**);
+    void validate(int, const char**);
     void init(char**);
     void concatanate(char**, const char**, const int);
 
